8324c6ff0aad5623e6997212cc78e35782c23f3a
Kétio <krembaux@gmail.com>
2025-10-07 17:11:10 +0200

    Update project_TEXTES.py

---
+++ project_TEXTES.py
@@ -0,0 +1,21 @@
+"Bienvenue sur AKBank"
+
+"Entrez votre identifiant :"
+"Votre identifient est invalide. Réessayez :"
+
+"Entrez votre mot de passe :"
+"Votre mot de passe est invalide. Réessayez :"
+
+"Bonjour M. ou Mme. ?.\n"
+"Tapez {1} pour consulter votre solde.\n"
+"Tapez {2} pour retirer de l'argent.\n"
+"Tapez {3} pour déposer de l'argent."
+
+"Vous avez actuellement {?} € sur votre compte."
+
+"Tapez le montant que vous voulez retirer :"
+"Vous êtes trop pauvre ce montant. Réessayez :"
+
+"Voulez vous retirer en billets de 5, 10, 20 ou 50 € ?"
+
+"Tapez le montant que vous voulez déposer :"
