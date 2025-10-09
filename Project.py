7a99d80c03cadda4b54340b44f188684b5d75037
Andrianchick <andriankostenko@gmail.com>
2025-10-09 11:17:47 +0200

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,20 +1,7 @@
-print ("w")
+print ("Bienvenue sur AKBank")
+var_id = input("Entrez votre identifiant :")
+var_pin = input("Entrez votre identifiant :")
 id = "Tralalelo"
 pin = " " "Tralala"
 total = id + pin
-
-print (pin + " est correcte?")
-b = input ("Oui ou Non tapez o ou n")
-if b == "o":
-    print ("bugaga")
-    a = input()
-    if:
-     a == total 
-     print ("halal porc")
-    
-elif b == "O":
-    print ("bugaga")
-else: 
-    print ("Kiknis is jizni eblan")
-
 
