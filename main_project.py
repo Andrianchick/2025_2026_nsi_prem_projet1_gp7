788f75f4107215b6bf6b5a55612201e6e3049926
Kétio <krembaux@gmail.com>
2025-10-09 12:10:14 +0200

    Update main_project.py

---
+++ main_project.py
@@ -0,0 +1,11 @@
+client : {
+    123 : {
+        "nom" : ,
+        "prénom" : Ana,
+        "solde" : ,
+        "dépots" : [ {"montant" : 587, "date" : "15-05-2025"},
+                     {"montant" : 5468, "date" : "21-07-2025"} ],
+        "retraits" : [ {"montant" : 845, "date" : "19-01-2025"},
+                     {"montant" : 940, "date" : "29-07-2025"} ],
+    },
+}
