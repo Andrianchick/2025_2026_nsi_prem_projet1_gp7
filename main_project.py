380ebabb2c7e58234862839f76b20837822581fd
Kétio <krembaux@gmail.com>
2025-10-14 16:19:32 +0200

    Update main_project.py

--- main_project.py
+++ main_project.py
@@ -1,11 +1,15 @@
 client : {
-    123 : {
-        "nom" : ,
-        "prénom" : Ana,
-        "solde" : ,
-        "dépots" : [ {"montant" : 587, "date" : "15-05-2025"},
-                     {"montant" : 5468, "date" : "21-07-2025"} ],
-        "retraits" : [ {"montant" : 845, "date" : "19-01-2025"},
-                     {"montant" : 940, "date" : "29-07-2025"} ],
-    },
+{ 123 : 
+ { "nom" : "Tralalero",
+  "prénom" : "Ana",
+  "solde" : 546.465,
+  "dépots" : [ {"montant" : 587, "date" : "15-05-2025"}, {"montant" : 5468, "date" : "21-07-2025"} ],
+  "retraits" : [ {"montant" : 845, "date" : "19-01-2025"}, {"montant" : 940, "date" : "29-07-2025"} ], },
+  321 :
+  { "nom" : "Tralala",
+   "prénom" : "Carole",
+   "solde" : 78.465,
+   "dépots" : [ {"montant" : 546, "date" : "15-05-2025"}, {"montant" : 456, "date" : "21-07-2025"} ],
+   "retraits" : [ {"montant" : 458, "date" : "19-01-2025"}, {"montant" : 789, "date" : "29-07-2025"} ], },
+}
 }
