6ef5b0fb728c9332d2c0922519e172a75e3695bc
Kétio <krembaux@gmail.com>
2025-11-04 16:50:59 +0100

    Update mimimoise.py

--- mimimoise.py
+++ mimimoise.py
@@ -1,6 +1,82 @@
 import json
 from datetime import datetime
-clients = ""
+clients = {
+    "123": {
+        "identifiant": "Tralalero",
+        "mot_de_passe": 1111,
+        "solde": 52465,
+        "depots": [
+            {
+                "montant": 845,
+                "date": "14/01/2024"
+            },
+            {
+                "montant": 940,
+                "date": "03/02/2024"
+            }
+        ],
+        "retraits": [
+            {
+                "montant": 354,
+                "date": "27/03/2024"
+            },
+            {
+                "montant": 540,
+                "date": "09/04/2024"
+            }
+        ]
+    },
+    "321": {
+        "identifiant": "Bombardiro",
+        "mot_de_passe": 2222,
+        "solde": 2146,
+        "depots": [
+            {
+                "montant": 546,
+                "date": "18/05/2024"
+            },
+            {
+                "montant": 456,
+                "date": "06/06/2024"
+            }
+        ],
+        "retraits": [
+            {
+                "montant": 458,
+                "date": "22/07/2024"
+            },
+            {
+                "montant": 789,
+                "date": "11/08/2024"
+            }
+        ]
+    },
+    "231": {
+        "identifiant": "Ballerina",
+        "mot_de_passe": 3333,
+        "solde": 45308,
+        "depots": [
+            {
+                "montant": 286,
+                "date": "30/09/2024"
+            },
+            {
+                "montant": 84,
+                "date": "05/10/2024"
+            }
+        ],
+        "retraits": [
+            {
+                "montant": 68,
+                "date": "16/11/2024"
+            },
+            {
+                "montant": 57,
+                "date": "28/12/2024"
+            }
+        ]
+    }
+}
 with open("clients.json", "w") as f:
     json.dump(clients, f, indent=4, ensure_ascii=False)
 
